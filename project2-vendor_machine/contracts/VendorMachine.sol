9f59f8e0717622146e491ad0dd9e02f8123e064d
“Paulo <paulocol@gmail.com>
2023-05-04 00:35:57 +0100

    Vendor Machine
    Finishing the smart contract for the Vendor Machine project, and increasing the test coverage to 100%.

--- project2-vendor_machine/contracts/VendorMachine.sol
+++ project2-vendor_machine/contracts/VendorMachine.sol
@@ -5,10 +5,15 @@ import "hardhat/console.sol";
 
 contract VendorMachine {
     
+    address owner;
+
     mapping(uint16 => uint) public productPrice;
-    mapping(uint16 => uint) public productQty;
+    mapping(uint16 => uint16) public productQty;
 
     constructor() {
+
+        owner = msg.sender;
+
         productPrice[1] = 0.1 ether;
         productQty[1] = 10;
         productPrice[2] = 0.2 ether;
@@ -18,18 +23,45 @@ contract VendorMachine {
         productPrice[4] = 0.4 ether;
         productQty[4] = 10;
         productPrice[5] = 0.5 ether;
-        productQty[5] = 10;
+        productQty[5] = 1;
     }
     
     function buy(uint16 product) external payable {
-        console.log(productQty[product]);
         require(productQty[product] > 0, "Product unavailable!");
         require(msg.value == productPrice[product], "Incorrect value!");
         productQty[product]--;      
-        console.log(productQty[product]);
+    }
+
+    function addStock(uint16 product, uint16 quantity) external {
+        require(msg.sender == owner, "You don't have permission!");
+        require(product > 0 && product <= 5, "Invalid product!");
+        require(quantity > 0, "Invalid quantity!");
+        productQty[product] += quantity;
+    }
+
+    function changePrice(uint16 product, uint newPrice) external {
+        require(msg.sender == owner, "You don't have permission!");
+        require(product > 0 && product <= 5, "Invalid product!");
+        require(newPrice > 0, "Invalid price!");
+        productPrice[product] = newPrice;
+    }
+
+    function withdraw() external payable {
+        require(msg.sender == owner, "You don't have permission!");
+        uint balance = address(this).balance;
+        require(balance > 0, "No balance to withdraw!");
+        payable(owner).transfer(balance);
     }
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    function getStock(uint16 product) public view returns (uint16) {
+        return productQty[product];
+    }
+
+    function getPrice(uint16 product) public view returns (uint) {
+        return productPrice[product];
     }
 }
--- project2-vendor_machine/test/VendorMachine.test.ts
+++ project2-vendor_machine/test/VendorMachine.test.ts
@@ -19,9 +19,127 @@ describe("VendorMachine", function () {
 
         await instance.buy(3, {value: paidValue});
 
-        console.log(await contract.getBalance());
-
         expect(await contract.getBalance() == paidValue);
     });
+
+    it("Try to buy a product that is not available", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const instance = contract.connect(accounts[1]);
+        const paidValue = ethers.utils.parseEther("0.5");
+
+        await instance.buy(5, {value: paidValue});
+
+        await expect(instance.buy(5, {value: paidValue})).to.be.revertedWith("Product unavailable!");
+    });
+
+    it("Try to buy a product with wrong value", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const instance = contract.connect(accounts[1]);
+        const paidValue = ethers.utils.parseEther("0.2");
+
+        await expect(instance.buy(5, {value: paidValue})).to.be.revertedWith("Incorrect value!");
+    });
+
+    it("Add stock", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const quantity = await contract.getStock(3);
+
+        await contract.addStock(3, 1);
+
+        expect(await contract.getStock(3) == (quantity + 1));
+    });
+
+    it("Try to add stock without permission", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const instance = contract.connect(accounts[1]);
+
+        await expect(instance.addStock(1, 1)).to.be.revertedWith("You don't have permission!");
+    });
+
+    it("Try to add stock to an invalid product", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        await expect(contract.addStock(6, 1)).to.be.revertedWith("Invalid product!");
+    });
+
+    it("Try to add stock to an invalid quantity", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        await expect(contract.addStock(1, 0)).to.be.revertedWith("Invalid quantity!");
+    });
+
+    it("Change price", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const newPrice = ethers.utils.parseEther("0.11");
+
+        await contract.changePrice(1, newPrice);
+
+        expect(await contract.getPrice(3) == newPrice);
+    });
+
+    it("Try to change price without permission", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const newPrice = ethers.utils.parseEther("0.11");
+
+        const instance = contract.connect(accounts[1]);
+
+        await expect(instance.changePrice(1, newPrice)).to.be.revertedWith("You don't have permission!");
+    });
+
+    it("Try to change price to an invalid product", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const newPrice = ethers.utils.parseEther("0.11");
+
+        await expect(contract.changePrice(6, newPrice)).to.be.revertedWith("Invalid product!");
+    });
+
+    it("Try to change price to an invalid price", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const newPrice = ethers.utils.parseEther("0");
+        
+        await expect(contract.changePrice(1, newPrice)).to.be.revertedWith("Invalid price!");
+    });   
+
+    it("Withdraw", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+        
+        const paidValue = ethers.utils.parseEther("0.3");
+
+        const instance = contract.connect(accounts[1]);
+        await instance.buy(3, {value: paidValue});
+    
+        await contract.withdraw();
+
+        const newbalance = ethers.utils.parseEther("0");
+
+        expect(await contract.getBalance() == newbalance);
+    });
+
+
+    it("Try to withdraw without permission", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+                
+        const paidValue = ethers.utils.parseEther("0.3");
+
+        const instance = contract.connect(accounts[1]);
+        await instance.buy(3, {value: paidValue});
+    
+        await expect(instance.withdraw()).to.be.revertedWith("You don't have permission!");
+    });
+
+    it("Try to withdraw without balance", async () => {
+        const { contract, accounts } = await loadFixture(deployFixture);
+                    
+        await expect(contract.withdraw()).to.be.revertedWith("No balance to withdraw!");
+    });
+
     
 });
